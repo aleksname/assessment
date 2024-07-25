c2984f3654cfa79cb96c06c6654a98c69815590c
aleksname <aleksname.v2@gmail.com>
2024-07-25 23:03:30 +0300

    fix: IDK why i can.t deploy Diagrama page, I past old version

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0203f1f823f95059"; }
+/******/ 		__webpack_require__.h = function() { return "56d21e2d08a09a8a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
