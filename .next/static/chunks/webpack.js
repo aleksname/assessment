f357f47ac2ba01f78f2ff75f63f6e4dbb9f8bc71
aleksname <aleksname.v2@gmail.com>
2024-08-01 11:41:28 +0300

    fix: pull last version

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ad081d8a56971dc0"; }
+/******/ 		__webpack_require__.h = function() { return "6c4a24bf6161ea3a"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
